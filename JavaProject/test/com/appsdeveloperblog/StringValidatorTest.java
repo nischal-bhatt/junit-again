5acf5475150aa7ab5f70e91075444fa64f9b820a
nischal-bhatt <nischal.bhatt.87@gmail.com>
2023-03-17 23:32:31 +0800

    parameteriszed tests

--- JavaProject/test/com/appsdeveloperblog/StringValidatorTest.java
+++ JavaProject/test/com/appsdeveloperblog/StringValidatorTest.java
@@ -1,9 +1,11 @@
 package com.appsdeveloperblog;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -50,6 +52,26 @@ class StringValidatorTest {
 
       assertEquals("hey",exception.getMessage());
     }
+
+    @DisplayName("hi")
+    @ParameterizedTest
+    @MethodSource("balle")
+    void intsub(String x, boolean a)
+    {
+           assertEquals(stringValidator.validateString(x),a);
+    }
+
+    private static Stream<Arguments> balle()
+    {
+
+        return Stream.of(
+              Arguments.of("nish",false),
+              Arguments.of("chanikyarc",true)
+        );
+
+    }
+
+
 
 
 }
